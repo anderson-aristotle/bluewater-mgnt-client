972135257ffc91aef64c4e34b7bc2c298dd67d8d
rex <anderson.aristotle@gmail.com>
2019-04-14 17:17:17 -0400

    backend connected to front end

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://bucketlist-app-api.herokuapp.com',
+  production: 'https://bluewater-mgnt-api.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
